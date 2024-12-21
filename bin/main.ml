2a6ee1029a6a378e24dd4317d77182ddc4fe0bf1
Illia Mikhnevych <macbook@MacBook-Pro.local>
2024-12-21 14:22:11 +0200

    play with monadic exprs

--- bin/main.ml
+++ bin/main.ml
@@ -1,11 +1,10 @@
 let () =
+  let ( let* ) x f = Result.bind x f in
   let path = Sys.argv.(1) in
   let input = In_channel.input_all In_channel.stdin in
-  let path = Jqml.Path.parse path in
-  let res =
-    Result.bind path (fun path -> Yojson.Safe.from_string input |> Jqml.Path.access path)
-  in
-  match res with
-  | Ok res -> Format.printf "%s\n" @@ Yojson.Safe.pretty_to_string res
-  | Error err -> Format.printf "Error: %s\n" err
+  (let* path = Jqml.Path.parse path in
+   let* res = Yojson.Safe.from_string input |> Jqml.Path.access path in
+   Format.printf "%s\n" @@ Yojson.Safe.pretty_to_string res;
+   Ok ())
+  |> Result.iter_error (fun err -> Format.printf "Error: %s\n" err)
 ;;
