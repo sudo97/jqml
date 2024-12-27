89384aaa069abd5abd6f1ba0679ee8ecd2970679
Illia Mikhnevych <macbook@MacBook-Pro.local>
2024-12-27 13:08:55 +0200

    migrate to base

--- bin/main.ml
+++ bin/main.ml
@@ -1,10 +1,13 @@
+open Base
+
+let ( let* ) = Result.( >>= )
+
 let () =
-  let ( let* ) x f = Result.bind x f in
-  let path = Sys.argv.(1) in
+  let path = (Sys.get_argv ()).(1) in
   let input = In_channel.input_all In_channel.stdin in
   (let* path = Jqml.Path.parse path in
    let* res = Yojson.Safe.from_string input |> Jqml.Path.access path in
-   Format.printf "%s\n" @@ Yojson.Safe.pretty_to_string res;
+   Stdio.printf "%s\n" @@ Yojson.Safe.pretty_to_string res;
    Ok ())
-  |> Result.iter_error (fun err -> Format.printf "Error: %s\n" err)
+  |> Result.iter_error ~f:(fun err -> Stdio.eprintf "Error: %s\n" err)
 ;;
--- lib/path.ml
+++ lib/path.ml
@@ -1,3 +1,5 @@
+open Base
+
 type t =
   | Arr of int
   | Obj of string
@@ -5,27 +7,24 @@ type t =
   | ForEach of t
 
 let rec access path (json : Yojson.Safe.t) =
-  let recur acc err v =
-    v |> Option.to_result ~none:err |> Fun.flip Result.bind (access acc)
-  in
+  let recur acc err v = Result.(v |> of_option ~error:err >>= access acc) in
   match path, json with
   | [], _ -> Ok json
   | Obj key :: rest, `Assoc fields ->
-    List.assoc_opt key fields |> recur rest "Field not found"
-  | Arr n :: rest, `List lst -> List.nth_opt lst n |> recur rest "Index out of bounds"
-  | ArrSlice (start, end_) :: [], `List lst ->
-    Ok
-      (`List
-          (lst |> List.to_seq |> Seq.drop start |> Seq.take (end_ - start) |> List.of_seq))
+    List.find_map fields ~f:(fun (k, v) -> if String.(k = key) then Some v else None)
+    |> recur rest "Field not found"
+  | Arr n :: rest, `List lst -> List.nth lst n |> recur rest "Index out of bounds"
+  | [ ArrSlice (start, end_) ], `List lst ->
+    Ok (`List (List.take (List.drop lst start) (end_ - start)))
   | ArrSlice (_, _) :: _, _ -> Error "TODO: array slice can only be the last path for now"
   | ForEach _ :: _, _ -> Error "Not implemented"
-  | _ -> Error "Invalid path, the accessor and the json are not compatible"
+  | _ -> Error "Invalid path"
 ;;
 
 let parse =
   let open Angstrom in
   let dot = char '.' in
-  let key = take_while1 (fun c -> c <> '.' && c <> '[' && c <> ']') in
+  let key = take_while1 (fun c -> Char.(c <> '.' && c <> '[' && c <> ']')) in
   let integer =
     take_while1 (function
       | '0' .. '9' -> true
@@ -33,7 +32,7 @@ let parse =
   in
   let obj = dot *> key >>| fun key -> Obj key in
   let full_slice =
-    (fun start end_ -> ArrSlice (int_of_string start, int_of_string end_))
+    (fun start end_ -> ArrSlice (Int.of_string start, Int.of_string end_))
     <$> char '[' *> integer
     <*> (char ':' *> integer <* char ']')
   in
@@ -41,12 +40,12 @@ let parse =
     char '[' *> integer
     <* char ':'
     <* char ']'
-    >>| fun n -> ArrSlice (int_of_string n, Int.max_int)
+    >>| fun n -> ArrSlice (Int.of_string n, Int.max_value)
   in
   let slice_no_min =
-    char '[' *> char ':' *> integer <* char ']' >>| fun n -> ArrSlice (0, int_of_string n)
+    char '[' *> char ':' *> integer <* char ']' >>| fun n -> ArrSlice (0, Int.of_string n)
   in
-  let arr = char '[' *> integer <* char ']' >>| fun n -> Arr (int_of_string n) in
+  let arr = char '[' *> integer <* char ']' >>| fun n -> Arr (Int.of_string n) in
   let path =
     many1 (obj <|> arr <|> full_slice <|> slice_no_max <|> slice_no_min)
     <|> fail "invalid form"
